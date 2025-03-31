6512bbe7f8f8e2cb2e28fdbae693a3278fd680ed
unknown <ouatpaulo@gmail.com>
2025-03-31 23:37:01 +0000

    Suppression de venv/ et .history/ du suivi Git

--- .history/project/chatbot/urls_20250331225536.py
+++
@@ -1,6 +0,0 @@
-﻿from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chatbot_view, name='chatbot'),
-]
--- .history/project/config/urls_20250331225607.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin,inclu
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
-]
